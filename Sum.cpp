05e672e0b8763e3f81eebde1dbb4ddf98a09e095
Bhavya Pathak <89476775+bhavyapathak20@users.noreply.github.com>
2021-10-14 16:14:05 +0530

    Add files via upload

---
+++ Sum.cpp
@@ -0,0 +1,25 @@
+//input the numbers from the user and print the sum.
+
+#include<iostream>
+using namespace std;
+
+int main()
+ {
+    int num1, num2;
+ 
+    cout<<"Enter the value for num1:\n";
+    // "<<" is called insertion operator.
+    cin>>num1;
+    // ">>" is called extraction operator.
+
+
+
+    cout<<"Enter the value for num2:\n";
+    cin>>num2;
+
+    cout<<"The sum is:"<< num1+num2;
+    num1+num2;
+    
+
+    return 0;
+}
